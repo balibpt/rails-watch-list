846789e4f0f598093e8aeca1a4413c990f94f1b4
Bali Tok <bali.tok@hotmail.com>
2023-03-10 11:59:14 +0800

    Changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'lists#index', as: :lists
+  root 'lists#index'
 
   resources :lists, except: [:index] do
     resources :bookmarks, only: %i[new create]
