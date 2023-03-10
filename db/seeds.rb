5290cf358325824d68e602f6560ac9953fae9272
Bali Tok <bali.tok@hotmail.com>
2023-03-10 17:22:13 +0800

    Modified Frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+require 'net/http'
+require 'json'
 
 puts 'Deleting Movies...........'
 Movie.destroy_all
 
 puts 'Creating Movies.....'
-6.times do
-  Movie.create!(title: Faker::Movie.title, overview: Faker::Movie.quote,
-                poster_url: 'https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg',
-                rating: (6..10).to_a.sample)
+
+url = URI('https://tmdb.lewagon.com/movie/top_rated')
+movies_serialized = Net::HTTP.get(url)
+movies = JSON.parse(movies_serialized)
+movies['results'].each do |result|
+  Movie.create!(title: result['title'], overview: result['overview'], poster_url: "https://image.tmdb.org/t/p/w500#{result['poster_path']}",
+                rating: result['vote_average'])
 end
 
 puts 'Created!'
